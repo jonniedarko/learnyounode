091d4ab007370e6c253fea671461ade3f9926a6d
jonniedarko <jonnie.maguire@gmail.com>
2014-01-07 15:27:35 +0000

    Create 11-Duplexer.js

---
+++ steam-adventure/solutions/11-Duplexer.js
@@ -0,0 +1,10 @@
+(function(){
+	var spawn = require("child_process").spawn;
+	var duplexer = require("duplexer");
+
+	module.exports = function (cmd, args){
+		var ps = spawn(cmd, args);
+		return duplexer(ps.stdin, ps.stdout);
+	};
+
+})();
